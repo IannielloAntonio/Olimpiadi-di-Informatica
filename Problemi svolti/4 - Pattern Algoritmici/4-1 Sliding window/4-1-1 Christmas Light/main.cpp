2ccd25b9c3797e735ccf9921818be2795f08805c
IannielloAntonio <ianniello.antonio@sobrero.it>
2026-01-27 15:52:45 +0100

    Christmas Light

--- Problemi svolti/4 - Pattern Algoritmici/4-1 Sliding window/4-1-1 Christmas Light/main.cpp
+++ Problemi svolti/4 - Pattern Algoritmici/4-1 Sliding window/4-1-1 Christmas Light/main.cpp
@@ -9,7 +9,9 @@ int main()
       cin >> N;
       cin >> C;
 
-      vector<int> count(C, 0);
+      vector<int> count(C, 0); // count è il vettore che contiene quante luci accese ci sono per ogni colore.
+                              // Nella posizione 1, ad esempio, se c'è il numero 2, vuol dire che ci sono
+                              // 2 luci accesi del colore 1.
       vector<int64_t> L(N);
 
       for (int i = 0; i < N; i++)
@@ -32,20 +34,22 @@ int main()
                   total_color++;
             }
 
-            while (total_color == C) // nella sliding window il while è un classico
+            while (total_color == C) // nella sliding window variabile il while è un classico
                                     // quando è rispettata la condizione (o non rispettata, 
                                     // a seconda del problema), si riduce la finestra con "left++"
             {
                   dist = min(dist, right - left + 1);
 
                   
-                  count[L[left]]--;
+                  count[L[left]]--; // stiamo per restringere la finestra, quindi andiamo a togliere il colore corrispondente
+                                    // all'elemento uscente.
                   
-                  if(count[L[left]] == 0){
+                  if(count[L[left]] == 0){ // se il contatore relativo a quel colore arriva a 0, allora il numero totale
+                                          // di colori diminuisce.
                         total_color--;
                   }
 
-                  left++;
+                  left++; // contrazione della finestra.
                   
             }
 
