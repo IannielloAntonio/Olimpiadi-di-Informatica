3ebc6c5baed463bb24fa90963c8402169dd811ef
IannielloAntonio <ianniello.antonio@sobrero.it>
2026-02-24 16:50:00 +0100

    Aggiunto problema book shop da finire

---
+++ Libro/Problemi svolti/6 - Programmazione Dinamica/Programmazione Dinamica Ricorsiva/Book Shop/main.cpp
@@ -0,0 +1,56 @@
+// https://cses.fi/problemset/task/1158
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> s;
+vector<int> h;
+vector<vector<int>> memo;
+
+int dp(int stato, int budget){
+      if(budget <= 0 || stato == n){
+            return 0;
+      }
+
+      if(memo[stato][budget] != -1){
+            return memo[stato][budget];
+      }
+
+
+      int preso=0, non_preso=0;
+
+      if(budget >= h[stato]){
+            preso = dp(stato+1, budget-h[stato]) + s[stato];
+      }
+     
+      non_preso = dp(stato+1, budget);
+
+      memo[stato][budget] = max(preso, non_preso);
+      return memo[stato][budget];
+
+      
+}
+
+int main() {
+      int x;
+      cin >> n >> x;
+
+      s = vector<int>(n);
+      h = vector<int>(n);
+
+      for(int i=0; i<n; i++){
+            cin >> h[i];
+      }
+
+      for(int i=0; i<n; i++){
+            cin >> s[i];
+      }
+
+      memo = vector<vector<int>>(n, vector<int>(100001, -1));
+
+      cout << dp(0, x);
+
+
+      return 0;
+}
